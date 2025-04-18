156553af0431a500f7accb84646354a6c843f16b
bonnie <owu.owu.666@gmail.com>
2025-04-18 15:55:23 -0400

    finished hoot details

--- src/components/HootDetails/HootDetails.jsx
+++ src/components/HootDetails/HootDetails.jsx
@@ -1,47 +1,57 @@
-import { useParams } from 'react-router';
-import { useState, useEffect } from 'react';
+import { useParams } from "react-router";
+import { useState, useEffect } from "react";
 // import * as hootService from '../../services/hootService';
 
-
 const HootDetails = () => {
-    const { hootId } = useParams();
-    console.log('hootId', hootId);
+  const { hootId } = useParams();
+  console.log("hootId", hootId);
 
-    const [hoot, setHoot] = useState(null)
+  const [hoot, setHoot] = useState(null);
 
-    useEffect(() => {
-        setHoot({ 
-            category: "This is a category", 
-            title: "Title", 
-            author: {
-                username: "ra"
-            },
-            createdAt: "date", 
-            text: "this is text"
-        })
-    }, [])
+  useEffect(() => {
+    setHoot({
+      category: "This is a category",
+      title: "Title",
+      author: {
+        username: "ra",
+      },
+      createdAt: "date",
+      text: "this is text",
+    });
+  }, []);
 
+  if (!hoot) return <main>Loading...</main>;
 
-    if (!hoot) return <main>Loading...</main>;
-
-    return (
-        <main>
-        <section>
-          <header>
-            <p>{hoot.category.toUpperCase()}</p>
-            <h1>{hoot.title}</h1>
-            <p>
-              {`${hoot.author.username} posted on
+  return (
+    <main>
+      <section>
+        <header>
+          <p>{hoot.category.toUpperCase()}</p>
+          <h1>{hoot.title}</h1>
+          <p>
+            {`${hoot.author.username} posted on
               ${new Date(hoot.createdAt).toLocaleDateString()}`}
-            </p>
-          </header>
-          <p>{hoot.text}</p>
-        </section>
-        <section>
-          <h2>Comments</h2>
-        </section>
-      </main>
-    );
+          </p>
+        </header>
+        <p>{hoot.text}</p>
+      </section>
+      <section>
+        <h2>Comments</h2>
+        {!hoot.comments.length && <p>There are no comments.</p>}
+        {hoot.comments.map((comment) => (
+          <article key={comment._id}>
+            <header>
+              <p>
+                {`${comment.author.username} posted on
+        ${new Date(comment.createdAt).toLocaleDateString()}`}
+              </p>
+            </header>
+            <p>{comment.text}</p>
+          </article>
+        ))}
+      </section>
+    </main>
+  );
 };
 
-  export default HootDetails;
+export default HootDetails;
